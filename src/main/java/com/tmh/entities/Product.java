bbc3eb35ae9c50b4cd63df6e3a0aad5ee3333656
bright0709 <bui.hoang.minh@sun-asterisk.com>
2019-07-19 10:55:28 +0700

    Create Entities

---
+++ src/main/java/com/tmh/entities/Product.java
@@ -0,0 +1,59 @@
+package com.tmh.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<OrderItem> orderItems;
+
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "unit_price")
+	private float unitPrice;
+	
+	@Column(name = "image")
+	private String image;
+	
+	@Column(name = "created_at")
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	
+	@Column(name = "updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
+	
+}
---
+++ src/main/java/com/tmh/entities/User.java
@@ -0,0 +1,59 @@
+package com.tmh.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Order> orders;
+	
+	@Column(name = "full_name")
+	private String fullName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "role")
+	private int role;
+	
+	@Column(name = "created_at")
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	
+	@Column(name = "updated_at")
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
+	
+}
