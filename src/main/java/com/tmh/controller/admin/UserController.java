2a0cef32dfbcb0a6ca4f3dbdc3cbf0296c550027
bright0709 <bui.hoang.minh@sun-asterisk.com>
2019-07-24 09:17:38 +0700

    handle user CRUD on admin page

--- src/main/java/com/tmh/controller/admin/UserController.java
+++ src/main/java/com/tmh/controller/admin/UserController.java
@@ -4,8 +4,14 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.tmh.entities.User;
 import com.tmh.service.UserService;
 
 @Controller
@@ -17,10 +23,72 @@ public class UserController {
 	private static final Logger logger = Logger.getLogger(UserController.class);
 
 	@RequestMapping(value = "/users")
-	public String userList(Model model) {
-		logger.info("user list page");
+	public String showUserList(Model model) {
+		logger.info("show users list");
+		
 		model.addAttribute("users", userService.findAll());
+		
 		return "views/admin/userManager/userList";
+	}
+	
+	@RequestMapping(value = "/users/{id}")
+	public String showUserDetail(@PathVariable("id") int id, Model model) {
+		logger.info("show user detail");
+		
+		User user = userService.findById(id);
+		
+		if (user == null) {
+			model.addAttribute("css", "danger");
+			model.addAttribute("msg", "User Not Found");
+		}
+		
+		model.addAttribute("user", user);
+		
+		return "views/admin/userManager/userDetail";
+	}
+	
+	@RequestMapping(value = "/users/{id}/delete")
+	public String deleteUser(@PathVariable("id") int id, final RedirectAttributes redirectAttributes) {
+		logger.info("delete user");
+		
+		userService.delete(userService.findById(id));
+		
+		return "redirect:/users";
+	}
+	
+	@RequestMapping(value = "/users/add")
+	public String addNewUser(Model model) {
+		logger.info("add user");
+		
+		User user = new User();
+		user.setRole(0);
+		
+		model.addAttribute("userForm", user);
+		model.addAttribute("status", "add");
+		
+		return "views/admin/userManager/userForm";
+	}
+	
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	public String submitAddOrUpdateUser(@ModelAttribute("userForm") User user, BindingResult bindingResult, Model model) {
+		logger.info("submit add/update user");
+		
+		userService.saveOrUpdate(user);
+		model.addAttribute("user", user);
+		
+		return "views/admin/userManager/userDetail";
+	}
+	
+	@RequestMapping(value = "/users/{id}/edit")
+	public String editUser(@PathVariable("id") int id, Model model) {
+		logger.info("edit user");
+		
+		User user = userService.findById(id);
+		
+		model.addAttribute("userForm", user);
+		model.addAttribute("status", "edit");
+		
+		return "views/admin/userManager/userForm";
 	}
 	
 }
